61a916cdd2c8cf420e8c9fe8b67ab508d2c04885
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-01-22 14:33:30 -0300

    adição de mais lógicas e módulos

--- src/br/com/application/Main.java
+++ src/br/com/application/Main.java
@@ -1,11 +1,35 @@
 package br.com.application;
 
+import br.com.model.entities.ReservationHotel;
+
+import java.time.LocalDate;
 import java.util.Scanner;
+
+import static br.com.model.entities.ReservationHotel.fmt;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        LocalDate checkIn = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        LocalDate checkOut = LocalDate.parse(sc.next(), fmt);
+
+        ReservationHotel reservation = new ReservationHotel(number, checkIn, checkOut);
+        System.out.println("Reservation: " + reservation);
+
+        System.out.println();
+        System.out.println("Enter data to update the reservation:");
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        checkIn = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        checkOut = LocalDate.parse(sc.next(), fmt);
+
+        reservation.updateDates(checkIn, checkOut);
+        System.out.println("Reservation: " + reservation);
 
 
 
--- src/br/com/model/entities/ReservationHotel.java
+++ src/br/com/model/entities/ReservationHotel.java
@@ -1,11 +1,18 @@
 package br.com.model.entities;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class ReservationHotel {
     private Integer roomNumber;
     private LocalDate checkIn;
     private LocalDate checkOut;
+
+
+    public static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public ReservationHotel(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
         this.roomNumber = roomNumber;
@@ -32,7 +39,18 @@ public class ReservationHotel {
         return checkOut;
     }
     public void updateDates(LocalDate checkin, LocalDate checkout){
-        
+        this.checkIn = checkin;
+        this.checkOut = checkout;
+    }
+    public long duration() {
+        return Duration.between(checkIn.atStartOfDay(), checkOut.atStartOfDay()).toDays();
     }
 
+    @Override
+    public String toString() {
+        return String.format("Room - " + roomNumber +
+                ", check-in: " + checkIn.format(fmt) +
+                ", check-out: " + checkOut.format(fmt)) + ", " +
+                duration() + " nights.";
+    }
 }
