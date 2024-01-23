2d6359c4dd2a598632c2b69bd5278286080ddcf5
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-01-23 19:29:35 -0300

    Lógica da Main colocada em um while true

--- src/br/com/application/Main.java
+++ src/br/com/application/Main.java
@@ -11,46 +11,54 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ReservationHotel reservation = new ReservationHotel();
-        try {
-            System.out.print("Room number: ");
-            int number = sc.nextInt();
-            reservation.setRoomNumber(number);
-            System.out.print("Check-in date (dd/MM/yyyy): ");
-            LocalDate checkIn = LocalDate.parse(sc.next(), fmt);
-            System.out.print("Check-out date (dd/MM/yyyy): ");
-            LocalDate checkOut = LocalDate.parse(sc.next(), fmt);
-            /*
-             * Ou posso apagar a instanciação do construtor padrão e instanciar um construtor com argumentos:
-             * ReservationHotel reservation = new ReservationHotel(number, checkIn, checkOut);
-             * pois a lógica de validação da data também está no construtor
-             * */
-            reservation.updateDates(checkIn, checkOut);
-            System.out.println("Reservation: " + reservation);
-            System.out.println();
 
-            System.out.println("Do you want to update your reservation?(y/n)");
-            char ch = sc.next().toLowerCase().charAt(0);
+        boolean continua = true;
 
-            if (ch == 'y') {
-                System.out.println("Enter data to update the reservation:");
+            while (continua) {
+
+            try {
+                System.out.print("Room number: ");
+                int number = sc.nextInt();
+                reservation.setRoomNumber(number);
                 System.out.print("Check-in date (dd/MM/yyyy): ");
-                checkIn = LocalDate.parse(sc.next(), fmt);
+                LocalDate checkIn = LocalDate.parse(sc.next(), fmt);
                 System.out.print("Check-out date (dd/MM/yyyy): ");
-                checkOut = LocalDate.parse(sc.next(), fmt);
-
+                LocalDate checkOut = LocalDate.parse(sc.next(), fmt);
+                /*
+                 * Ou posso apagar a instanciação do construtor padrão e instanciar um construtor com argumentos:
+                 * ReservationHotel reservation = new ReservationHotel(number, checkIn, checkOut);
+                 * pois a lógica de validação da data também está no construtor
+                 * */
                 reservation.updateDates(checkIn, checkOut);
                 System.out.println("Reservation: " + reservation);
+                System.out.println();
+
+                System.out.println("Do you want to update your reservation?(y/n)");
+                char ch = sc.next().toLowerCase().charAt(0);
+
+                if (ch == 'n') {
+                    continua = false;
+                } else if (ch == 'y') {
+                    System.out.println("Enter data to update the reservation:");
+                    System.out.print("Check-in date (dd/MM/yyyy): ");
+                    checkIn = LocalDate.parse(sc.next(), fmt);
+                    System.out.print("Check-out date (dd/MM/yyyy): ");
+                    checkOut = LocalDate.parse(sc.next(), fmt);
+
+                    reservation.updateDates(checkIn, checkOut);
+                    System.out.println("Reservation: " + reservation);
+                }
             }
+            catch (DomainException e) {
+                System.out.println("Error in reservation: " + e.getMessage());
+            }
+            catch (RuntimeException e) {
+                System.out.println("Unexpected error");
+            }
+            /*
+            * Já existe uma exceção pronta no java para esta validação, que é a IllegalArgumentException e,
+            * porém a intenção deste cósigo é aprender a personalizar uma exceção própria
+            * */
         }
-        catch (DomainException e) {
-            System.out.println("Error in reservation: " + e.getMessage());
-        }
-        catch (RuntimeException e) {
-            System.out.println("Unexpected error");
-        }
-        /*
-        * Já existe uma exceção pronta no java para esta validação, que é a IllegalArgumentException e,
-        * porém a intenção deste cósigo é aprender a personalizar uma exceção própria
-        * */
     }
 }
